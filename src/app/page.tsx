6306855946b0980956ad5e74141a518633faaf4e
M s kulkarni <mskulkarni7486@gmailn.com>
2025-11-03 20:23:13 +0530

    create a project

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+const Page = () => {
+  return <div className="">
+    <Button variant='outline'>Hello</Button>
+  </div>;
+};
+export default Page;
