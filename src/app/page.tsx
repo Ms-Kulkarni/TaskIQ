3f408b9f9705aeb0e019c341982900cfd4fbc4fe
Ms-Kulkarni <mskulkarni7486@gmail.com>
2025-11-10 23:54:17 +0530

    03-trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import { Button } from "@/components/ui/button";
-const Page = () => {
-  return <div className="">
-    <Button variant='outline'>Hello</Button>
+import { caller } from '@/trpc/server';
+const Page = async () => {
+  const users = await caller.getUsers();
+  
+  return <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    {JSON.stringify(users)}
   </div>;
 };
-export default Page;
+export default Page; 
